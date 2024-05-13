8a12bb4de93b5b82cc55da1e950de5985c78e92a
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-05-13 16:16:04 +0300

    Update k6io.js

--- my_k6_project/scripts/k6io.js
+++ my_k6_project/scripts/k6io.js
@@ -2,10 +2,23 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+
 export const options = {
-  vus: 10,
-  duration: '15m',
-};
+  scenarios: {
+    example_scenario: { 
+      // Executor type to use https://grafana.com/docs/k6/latest/using-k6/scenarios/#scenario-executors
+      executor: 'shared-iterations',
+      // common scenario configuration
+      startTime: '10s',
+      gracefulStop: '5s',
+      env: { EXAMPLE_VAR: 'testing' },
+      tags: { example_tag: 'testing' },
+      // executor-specific configuration
+      vus: 10,     
+      duration: '15m',
+    }
+  }
+}
 
 export default function () {
   http.get('http://test.k6.io');

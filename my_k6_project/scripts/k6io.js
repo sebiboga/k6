ae5878ba0551b5cc02d0e7586e79679f32e0876d
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-05-13 15:53:09 +0300

    Update k6io.js

--- my_k6_project/scripts/k6io.js
+++ my_k6_project/scripts/k6io.js
@@ -2,6 +2,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 10,
+  duration: '15m',
+};
+
 export default function () {
   http.get('http://test.k6.io');
   sleep(1);
